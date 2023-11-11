5b9f0617e54dc375fb8aee7783c54b58336b8947
testman <testman@kompot.si>
2023-11-11 22:21:53 +0100

    add more meshes and textures

---
+++ game/actors/characters/generic_characters/azra_rat/azra_rat.gd
@@ -0,0 +1,18 @@
+extends CharacterBody3D
+
+@export var max_health = 30
+
+func wake_up():
+	$is_opponent.wake_up()
+
+func take_damage(amount):
+	$health_system.reduce_health(amount)
+	$is_opponent.paint_red()
+	#$is_opponent.draw_hit_sprite()
+	if not $is_opponent.awake:
+		wake_up()
+		
+func _on_health_system_health_depleted():
+	# TODO: stop logic, then play dead animation, then queue_free
+	$AnimationPlayer.play("ded")
+	#queue_free()
