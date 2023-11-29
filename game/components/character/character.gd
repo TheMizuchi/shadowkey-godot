895ab31e71dd731d14cae764492ecfe9fd48d0e1
testman <testman@kompot.si>
2023-11-29 21:42:41 +0100

    npc interaction

--- game/components/character/character.gd
+++ game/components/character/character.gd
@@ -1,11 +1,11 @@
 extends Node
 
+@export var prompt = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	add_to_group("interactable")
+	add_to_group("character")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func activate():
+	$quest_trigger.progress_related_quests()
