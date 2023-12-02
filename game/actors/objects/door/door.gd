eda737447f8698715f204ab9a0517ce75b462261
testman <testman@kompot.si>
2023-12-02 22:37:11 +0100

    add .gitignore

--- game/actors/objects/door/door.gd
+++ game/actors/objects/door/door.gd
@@ -1,12 +1,20 @@
 extends Node
 
 var is_open = false
+var collision_shape
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	collision_shape = $CollisionShape3D.shape
 
 func activate():
-	$mesh.position.x = -0.5
-	$mesh.position.z = -0.5
-	$mesh.rotation.y = deg_to_rad(90)
+	if is_open:
+		$CollisionShape3D.shape = collision_shape
+		$mesh.position.x = 0
+		$mesh.position.z = 0
+		$mesh.rotation.y = 0
+	else:
+		$mesh.position.x = -0.5
+		$mesh.position.z = -0.5
+		$mesh.rotation.y = deg_to_rad(90)
+		$CollisionShape3D.shape = null
