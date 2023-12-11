d605d19f7af1de7d5e2795c94cf0c6f7e32839d3
testman <testman@kompot.si>
2023-12-11 22:52:23 +0100

    small progress

--- game/game_logic/quest_tracking.gd
+++ game/game_logic/quest_tracking.gd
@@ -1,76 +1,53 @@
 extends Node
 
-# &"quest name": [current_quest_stage, [dialogs]]
+var quests = {}
 
 func _ready():
 	add_all_quests()
 
+# TODO: decide in rewards_dict structure
 class Quest:
 	var name
 	var current_stage = 0
+	var completion_stages = []
+	var stage_rewards = {}
+	var stage_dialogues = {}
+	var completed = false
 
 	func _init(quest_name):
 		name = quest_name
 	
+	func get_stage():
+		return current_stage
+	
 	func set_stage(stage):
 		current_stage = stage
+		if current_stage in completion_stages:
+			completed = true
+	
+	func add_stage_reward(stage, rewards):
+		stage_rewards[stage] = rewards
+	
+	func add_stage_dialogue(stage, dialogue):
+		stage_dialogues[stage] = dialogue
 	
 	func progress_quest():
 		current_stage += 1
+		if current_stage in completion_stages:
+			completed = true
 
-var new_quest_list = {}
-
-var quests = {&"Broken Wing Cameo" : [0, []], \
-	&"Bribery Papers" : [0, []], \
-	&"Blue Raider Mission" : [0, []], \
-	&"Collect Five Star Teeth" : [0, []], \
-	&"Clear Earthtear Caverns" : [0, []], \
-	&"Clerk Skye's Items" : [0, []], \
-	&"Caretaker Rescue" : [0, []], \
-	&"Disgruntled Thief" : [0, []], \
-	&"Dragonstar Gate Clear Hill" : [0, []], \
-	&"East Gate Raider Spree" : [0, []], \
-	&"Find Azra Nightwielder" : [0, []], \
-	&"Find the Temple" : [0, []], \
-	&"Goblin Rescue 1" : [0, []], \
-	&"Goblin Rescue 2" : [0, []], \
-	&"Gold Raider Mission" : [0, []], \
-	&"Herbs for Rilora" : [0, []], \
-	&"Herb Quest" : [0, []], \
-	&"Mage's Identity" : [0, []], \
-	&"Makor's Deal" : [0, []], \
-	&"Porliss Thieves' Guild" : [0, []], \
-	&"Porliss Loth' Na" : [0, []], \
-	&"Prisoner Assassin" : [0, []], \
-	&"Prisoner Family" : [0, []], \
-	&"Pilgrim Ghost Party" : [0, []], \
-	&"Pilgrim Ghost Ring" : [0, []], \
-	&"Porliss Caith Subterfuge" : [0, []], \
-	&"Porliss Lakvan Evidence" : [0, []], \
-	&"Prisoner Locket" : [0, []], \
-	&"Rescue Prisoners" : [0, []], \
-	&"Rilora Message" : [0, []], \
-	&"Red Raider Mission" : [0, []], \
-	&"Rogurin Letter of Introduction" : [0, []], \
-	&"Rat Quest" : [0, []], \
-	&"Skyrim Sympathizer's Map" : [0, []], \
-	&"Sergeant Deal" : [0, []], \
-	&"Sissithik's Will" : [0, []], \
-	&"Trailslag's Goods" : [0, []], \
-	&"Tanyin Lakvan Evidence" : [0, []], \
-	&"Tanyin Goblin Raid" : [0, []], \
-	&"Tanyin Mages' Guild" : [0, []], \
-	&"Temple Herbs" : [0, []], \
-	&"Tanyin Prisoner Free" : [0, []], \
-	&"Volstok Violet Resurrection" : [0, []], \
-	&"Witch Tree 1" : [0, []], \
-	&"Witch Tree 2" : [0, []], \
-	&"Witch Tree 3": [0, []]
-}
-
-func add_quest(id, name):
+func add_quest(id, name, completion_stages=[],\
+	stage_rewards={}, stage_dialogues={}):
 	var quest = Quest.new(name)
-	new_quest_list[id] = quest
+	for index in completion_stages:
+		quest.completion_stages.append(index)
+	for key in stage_rewards.keys():
+		quest.stage_rewards[key] = stage_rewards[key]
+	for key in stage_dialogues.keys():
+		quest.stage_dialogues[key] = stage_dialogues[key]
+		
+	
+	quests[id] = quest
 
 func add_all_quests():
 	add_quest(&"brokenwingcameo", "Broken Wing Cameo")
@@ -124,13 +101,14 @@ func get_quest_stage(quest_name):
 	return quests[quest_name][0]
 
 func set_quest_stage(quest_name, stage):
-	quests[quest_name][0] = stage
+	quests[quest_name].set_stage(stage)
 	print(quest_name, " ", stage)
 
 func progress_quest(quest_name):
 	# TODO: somehow check requirements for quest progress
 	if true:
-		set_quest_stage(quest_name, get_quest_stage(quest_name)+1)
+		quests[quest_name].progress_quest()
+		print(quests[quest_name].name, " is now at stage ", quests[quest_name].get_stage())
 		return true
 	return false
 	
